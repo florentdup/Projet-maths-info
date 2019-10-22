cf311d0e93f4d77fa8dae42e2c5e7f495c0086c5
florent <florent.dup512@sfr.fr>
2019-10-22 15:38:57 +0200

    5e commit

--- ligne-niveau.py
+++ ligne-niveau.py
@@ -67,14 +67,43 @@ plt.plot(X_l,Y_l,"*",color="black")
 
 
 
-def find_seed_4(g, c=0.0, eps=2**(-26)):
-    if (g(0,0)-c)*(g(0,1)-c)<0:
-        tmp=lambda x : g(0,x)-c  
-        return dichotomie(tmp,0,1,2**-26)
+def find_seed_4(g,x0,y0,x1,y1,c=0.0, eps=2**(-26)):
+    if (g(x0,y0)-c)*(g(x0,y1)-c)<0:
+        tmp=lambda x : g(x0,x)-c  
+        return x0,dichotomie(tmp,y0,y1,2**-26)
+    if (g(x0,y1)-c)*(g(x1,y1)-c)<0:
+        tmp=lambda x : g(x,y1)-c  
+        return dichotomie(tmp,x0,x1,2**-26),y1
+    if (g(x1,y1)-c)*(g(x1,y0)-c)<0:
+        tmp=lambda x : g(x1,x)-c  
+        return x1,dichotomie(tmp,y0,y1,2**-26)
+    if (g(x1,y0)-c)*(g(x0,y0)-c)<0:
+        tmp=lambda x : g(x,y0)-c  
+        return dichotomie(tmp,x0,x1,2**-26),y0
     else:
         return None
 
 def case(f,c,x0,y0,x1,y1,delta):
+    X,Y=[],[]
+    d0=find_seed(f,c)
+    if d0!=None:
+        x,y=0.0001,d0
+        gradient=grad_f(x,y)
+        s = 1
+        if gradient[1] < 0:
+            s = -1
+        while 0<=x<1 and 0<=y<1:
+            X.append(x)
+            Y.append(y)
+            gradient=grad_f(x,y)
+            n=[gradient[1],-gradient[0]]
+            norme=s*np.sqrt(n[0]**2+n[1]**2)
+            if norme!=0:
+                x+=delta/norme*n[0]
+                y+=delta/norme*n[1]
+            else:
+                break
+
     return X,Y
 
 
