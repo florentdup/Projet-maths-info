194d94ac77a162ad5a89bb79a424843c42bd2426
florent <florent.dup512@sfr.fr>
2019-10-28 14:36:20 +0100

    modif de find seed

--- ligne-niveau.py
+++ ligne-niveau.py
@@ -5,8 +5,11 @@ from scipy.optimize import bisect
 
 
 
-#f=lambda x,y:2*(np.exp(-x**2-y**2)-np.exp(-(x-1)**2-(y-1)**2))
-f=lambda x,y:x**2+y**2
+f=lambda x,y:2*(np.exp(-x**2-y**2)-np.exp(-(x-1)**2-(y-1)**2))
+#f=lambda x,y:x**2+y**2
+
+
+
 
 X,Y=np.linspace(-2,3,100),np.linspace(-1,2,100)
 
@@ -50,7 +53,6 @@ def simple_contour(f, c=0.0, delta=0.01):
         s = 1
         if gradient[1] < 0:
             s = -1
-        print(s)
         while 0<=x<1 and 0<=y<1:
             X.append(x)
             Y.append(y)
@@ -71,45 +73,47 @@ def simple_contour(f, c=0.0, delta=0.01):
 
 
 def find_seed_4(g,c,x0,y0,x1,y1, eps=2**(-26)):
+    eps=2**-26
+    res=[]
     if (g(x0,y0)-c)*(g(x0,y1)-c)<0:
-        tmp=lambda x : g(x0,x)-c  
-        return x0,dichotomie(tmp,y0,y1,2**-26)
+        tmp=lambda x : g(x0,x)-c
+        res.append((x0,dichotomie(tmp,y0,y1,eps)))
     if (g(x0,y1)-c)*(g(x1,y1)-c)<0:
-        tmp=lambda x : g(x,y1)-c  
-        return dichotomie(tmp,x0,x1,2**-26),y1
+        tmp=lambda x : g(x,y1)-c 
+        res.append((dichotomie(tmp,x0,x1,eps),y1))
     if (g(x1,y1)-c)*(g(x1,y0)-c)<0:
-        tmp=lambda x : g(x1,x)-c  
-        return x1,dichotomie(tmp,y0,y1,2**-26)
+        tmp=lambda x : g(x1,x)-c 
+        res.append((x1,dichotomie(tmp,y0,y1,eps)))
     if (g(x1,y0)-c)*(g(x0,y0)-c)<0:
-        tmp=lambda x : g(x,y0)-c  
-        return dichotomie(tmp,x0,x1,2**-26),y0
-    else:
-        return None,None
+        tmp=lambda x : g(x,y0)-c 
+        res.append((dichotomie(tmp,x0,x1,eps),y0))
+    return res
 
 def case(f,c,x0,y0,x1,y1,delta):
     X,Y=[],[]
-    x,y=find_seed_4(f,c,x0,y0,x1,y1)
-    if x!=None:
-        gradient=grad_f(x+0.00001,y+0.00001)
-        n=[gradient[1],-gradient[0]]
-        norme=np.sqrt(n[0]**2+n[1]**2)
-        if norme!=0:
-            x+=delta/norme*n[0]
-            y+=delta/norme*n[1]            
-        s = -1
-        if x0<=x<=x1 and y0<=y<=y1:
-            s = 1
-        while x0<=x<=x1 and y0<=y<=y1:
-            X.append(x)
-            Y.append(y)
+    seed=find_seed_4(f,c,x0,y0,x1,y1)
+    if seed!=[]:
+        for x,y in seed:
             gradient=grad_f(x+0.00001,y+0.00001)
             n=[gradient[1],-gradient[0]]
-            norme=s*np.sqrt(n[0]**2+n[1]**2)
+            norme=np.sqrt(n[0]**2+n[1]**2)
             if norme!=0:
                 x+=delta/norme*n[0]
-                y+=delta/norme*n[1]
-            else:
-                break
+                y+=delta/norme*n[1]            
+            s = -1
+            if x0<=x<=x1 and y0<=y<=y1:
+                s = 1
+            while x0<=x<=x1 and y0<=y<=y1:
+                X.append(x)
+                Y.append(y)
+                gradient=grad_f(x+0.00001,y+0.00001)
+                n=[gradient[1],-gradient[0]]
+                norme=s*np.sqrt(n[0]**2+n[1]**2)
+                if norme!=0:
+                    x+=delta/norme*n[0]
+                    y+=delta/norme*n[1]
+                else:
+                    break
     return X,Y
 
 
@@ -120,32 +124,31 @@ def contour(f, c=0.0, xc=[0.0,1.0], yc=[0.0,1.0], delta=0.01):
     for i in range(len(xc) - 1):
         for j in range(len(yc) - 1):
             a,b = case(f,c,xc[i],yc[j],xc[i+1],yc[j+1],delta)
-            X += a
-            Y += b
+
+            X.append(a)
+            Y.append(b)
 
     return X,Y
 
 
-case_x=100
-case_y=5
-delta=0.001
-X_l,Y_l=contour(f,0.0,list(np.linspace(-2,3,case_x)),list(np.linspace(-1,2,case_y)),delta)
-plt.plot(X_l,Y_l,".",color="black")
+case_x=12
+case_y=12
+delta=0.005
 
-X_l,Y_l=contour(f,0.5,list(np.linspace(-2,3,case_x)),list(np.linspace(-1,2,case_y)),delta)
-plt.plot(X_l,Y_l,".",color="black")
+liste_c=[-1.5,-1,-0.5,0.0,0.5,1.0,1.5]
 
-X_l,Y_l=contour(f,-0.5,list(np.linspace(-2,3,case_x)),list(np.linspace(-1,2,case_y)),delta)
-plt.plot(X_l,Y_l,".",color="black")
+for c in liste_c:
+    grille_x=list(np.linspace(-2,3,case_x))
+    grille_y=list(np.linspace(-1,2,case_y))
 
-X_l,Y_l=contour(f,1.5,list(np.linspace(-2,3,case_x)),list(np.linspace(-1,2,case_y)),delta)
-plt.plot(X_l,Y_l,".",color="black")
+    X_l,Y_l=contour(f,c,grille_x,grille_y,delta)
 
-X_l,Y_l=contour(f,-1.5,list(np.linspace(-2,3,case_x)),list(np.linspace(-1,2,case_y)),0.001)
-plt.plot(X_l,Y_l,".",color="black")
-#cont=plt.contour(Z,[0.01])
+    for x, y in zip(X_l, Y_l):
+        plt.plot(x, y,color="black")
 
- 
+ax = plt.gca()
+ax.set_aspect('equal')
 
 plt.show()
 
+    
